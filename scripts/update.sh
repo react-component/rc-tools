4c29208e0314c947b1ada43c7a9e88517025ada0
yiminghe <yiminghe@gmail.com>
2016-08-03 14:47:36 +0800

    update karma

---
+++ scripts/update.sh
@@ -0,0 +1,27 @@
+tnpm i karma --save
+
+tnpm i karma-cli --save
+
+tnpm i karma-chrome-launcher  --save
+
+tnpm i karma-coverage --save
+
+tnpm i karma-coveralls --save
+
+tnpm i karma-firefox-launcher --save
+
+tnpm i karma-ie-launcher --save
+
+tnpm i karma-mocha --save
+
+tnpm i karma-mocha-reporter --save
+
+tnpm i karma-phantomjs-launcher --save
+
+tnpm i karma-safari-launcher --save
+
+tnpm i karma-sauce-launcher --save
+
+tnpm i karma-sourcemap-loader --save
+
+tnpm i karma-webpack --save
