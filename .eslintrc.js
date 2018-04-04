7b22f7401ead3673f025d7b6689f8f6abe1f86d8
Wei Zhu <yesmeck@gmail.com>
2018-04-04 11:26:38 +0800

    Upgrade eslint and introduce prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  rules: {
+    'arrow-body-style': 0,
+    strict: 0,
+    'no-console': 0,
+    'func-names': 0,
+    'space-before-function-paren': 0,
+    'no-param-reassign': 0,
+    'import/no-dynamic-require': 0,
+    'global-require': 0,
+    'consistent-return': 0,
+  },
+};
