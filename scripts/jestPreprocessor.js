c461e605e4955d7c73c420c89a43dcec5bea25f9
zombieJ <smith3816@gmail.com>
2018-12-20 17:14:47 +0800

    Webpack4 & Babel7 & gulp4 (#61)
    * update babel
    
    * add tmp file generate
    
    * tmp webpack config
    
    * update deps
    
    * update babel
    
    * rename of webpack config
    
    * rm console
    
    * reorder
    
    * tmp process
    
    * auto detect processer
    
    * add david dm
    
    * add url
    
    * update deps

--- scripts/jestPreprocessor.js
+++ scripts/jestPreprocessor.js
@@ -1,22 +1,24 @@
-const typescript = require('typescript');
-const { createTransformer } = require('babel-jest');
+const path = require('path');
+const { createTransformer: babelTransFormer } = require('babel-jest');
+const { createTransformer: tsTransFormer } = require('ts-jest');
 const getBabelCommonConfig = require('../lib/getBabelCommonConfig');
-const getTSCommonConfig = require('../lib/getTSCommonConfig');
-const babelJest = createTransformer(getBabelCommonConfig());
+
+const tsJest = tsTransFormer({
+  tsConfig: path.join(__dirname, '../lib/tests/tsconfig.test.json'),
+});
+const babelJest = babelTransFormer(getBabelCommonConfig());
 
 module.exports = {
-  process(src, path) {
-    const isTypeScript = path.endsWith('.ts') || path.endsWith('.tsx');
-    const isJavaScript = path.endsWith('.js') || path.endsWith('.jsx');
+  process(src, filePath) {
+    const isTypeScript = filePath.endsWith('.ts') || filePath.endsWith('.tsx');
+    const isJavaScript = filePath.endsWith('.js') || filePath.endsWith('.jsx');
 
     if (isTypeScript) {
-      src = typescript.transpile(src, getTSCommonConfig(), path, []);
-    }
-
-    if (isJavaScript || isTypeScript) {
-      const fileName = path.replace(/\.[jt]sx?$/i, '.js');
-
-      src = babelJest.process(src, fileName, { moduleFileExtensions: ['js'] });
+      src = tsJest.process(src, filePath, { moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'] });
+    } else if (isJavaScript) {
+      src = babelJest.process(src, filePath, { moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'] });
+    } else {
+      throw new Error(`File not match type: ${filePath}`);
     }
 
     return src;
